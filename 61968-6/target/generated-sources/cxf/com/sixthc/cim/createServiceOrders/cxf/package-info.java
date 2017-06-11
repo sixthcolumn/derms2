503ea5dd22a84622c7180489fb64a4fddd46e504
Ben Goodwin <benj.goodwin@gmail.com>
2017-06-11 11:17:42 -0400

    Accidentally commited target dir

--- 61968-6/target/generated-sources/cxf/com/sixthc/cim/createServiceOrders/cxf/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://iec.ch/TC57/2015/ServiceOrdersMessage", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.sixthc.cim.createServiceOrders.cxf;
